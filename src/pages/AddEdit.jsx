54294b3efd49a088af7591c1c8a49c1957077963
Mukul Chhanikar <mukulchhanikar@gmail.com>
2021-10-26 19:11:23 +0530

    edit feature added

--- src/pages/AddEdit.jsx
+++ src/pages/AddEdit.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import firedb from '../firebase';
 import { toast } from 'react-toastify';
 import './styles.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 const initialState = {
     name: "",
@@ -14,26 +14,67 @@ const AddEdit = () => {
     const [data, setData] = useState({});
     const { name, email, contact } = state;
     const history = useHistory();
-    
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        firedb.child("contactDB").on("value", (snapshot) => {
+            if (snapshot.val() !== null) {
+                setData({ ...snapshot.val() });
+            }
+            else {
+                setData({});
+            }
+        });
+        return () => {
+            setData({});
+        }
+    }, [id]);
+
+    useEffect(() => {
+        if(id){
+            setState({...data[id]});
+        }
+        else{
+            setState({...initialState});
+        }
+        return() => {
+            setState({...initialState});
+        }
+    },[id, data])
+
     const inputChangeHandler = (event) => {
-        const {name, value} = event.target;
-        setState({...state, [name]: value});
+        const { name, value } = event.target;
+        setState({ ...state, [name]: value });
     }
 
     const submitHandler = (event) => {
         event.preventDefault();
-        if(!name || !email || !contact){
+        if (!name || !email || !contact) {
             toast.error("Fields cannot be empty...😔");
         }
-        else{
-            firedb.child("contactDB").push(state, (error) => {
-                if(error){
-                    toast.error(error);
-                }
-                else{
-                    toast.success("Contact added successfully...🙂")
-                }
-            });
+        else {
+            if(!id){
+                firedb.child("contactDB").push(state, (error) => {
+                    if (error) {
+                        toast.error(error);
+                    }
+                    else {
+                        toast.success("Contact added successfully...🙂")
+                    }
+                });
+            }
+            else{
+                firedb.child(`contactDB/${id}`).set(state, (error) => {
+                    if (error) {
+                        toast.error(error);
+                    }
+                    else {
+                        toast.success("Updated successfully...🙂")
+                    }
+                });
+            }
+
             setTimeout(() => history.push("/"), 500);
         }
     }
@@ -42,15 +83,15 @@ const AddEdit = () => {
         <div className="mt-5">
             <form className="form-body" onSubmit={submitHandler}>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" className="form-control" name="name" placeholder="Enter name" value={name} onChange={inputChangeHandler} />
-           
+                <input type="text" id="name" className="form-control" name="name" placeholder="Enter name" value={name || ""} onChange={inputChangeHandler} />
+
                 <label htmlFor="email" className="mt-4">Email</label>
-                <input type="email" id="email" className="form-control" name="email" placeholder="Enter Email" value={email} onChange={inputChangeHandler} />           
+                <input type="email" id="email" className="form-control" name="email" placeholder="Enter Email" value={email || ""} onChange={inputChangeHandler} />
 
                 <label htmlFor="contact" className="mt-4">Contact number</label>
-                <input type="text" id="contact" className="form-control" name="contact" placeholder="Enter contact number" value={contact} onChange={inputChangeHandler} maxLength={10} />
-            
-                <button type="submit" className="btn btn-success mt-4 w-100 btn-lg">Save Contact</button>
+                <input type="text" id="contact" className="form-control" name="contact" placeholder="Enter contact number" value={contact || ""} onChange={inputChangeHandler} maxLength={10} />
+
+                <button type="submit" className="btn btn-success mt-4 w-100 btn-lg">{id ? "Update" : "Save Contact"}</button>
             </form>
         </div>
     )
