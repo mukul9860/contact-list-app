ca5b1204c0bab91f361a39e6daa5c7555f8bc6a8
Mukul Chhanikar <mukulchhanikar@gmail.com>
2021-10-26 14:37:16 +0530

    links and header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Home from './pages/Home';
 import AddEdit from './pages/AddEdit';
 import View from './pages/View';
 import About from './pages/About';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Header />
+        <ToastContainer position="top-center" />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/add" component={AddEdit} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    const [activeTab, setTab] = useState("Home");
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setTab("Home");
+        } else if (location.pathname === "/add") {
+            setTab("AddContact");
+        } else if (location.pathname === "/about") {
+            setTab("About");
+        }
+    }, [location])
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-secondary p-0 ">
+                <Link className="navbar-brand ml-3" to="/">Contact App</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className=" collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav ml-auto mt-2 mr-5">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">
+                                <p
+                                    className={`${activeTab === "Home" ? "active" : "nav-link-text"}`}
+                                    onClick={() => setTab("Home")}>
+                                    Home</p>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/add">
+                                <p
+                                    className={`${activeTab === "AddContact" ? "active" : "nav-link-text"}`}
+                                    onClick={() => setTab("AddContact")}>
+                                    Add Contact</p>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">
+                                <p
+                                    className={`${activeTab === "About" ? "active" : "nav-link-text"}`}
+                                    onClick={() => setTab("About")}>
+                                    About</p>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
